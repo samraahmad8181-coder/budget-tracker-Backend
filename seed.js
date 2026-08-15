a4db74d297fe44ffd0f96492be71c66c6bb4904a
Samra Ahmad <samraahmad8181@gmail.com>
2026-08-15 03:29:25 -0700

    Add seed file

---
+++ seed.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+
+const pool = require("./src/db/db");
+
+async function seed() {
+    try {
+        await pool.query(
+            `INSERT INTO users (username, email, password, role)
+       VALUES ($1, $2, $3, $4)`,
+            [
+                "Admin",
+                "admin8862@gmail.com",
+                "$2b$10$QQjVB7xxq3wJZitTyGVjiOD.u09AiPaWg.6YuuBymnpGE7bdamC8a",
+                "admin",
+            ]
+        );
+
+        console.log("✅ Admin user inserted");
+    } catch (err) {
+        console.error(err);
+    } finally {
+        await pool.end();
+    }
+}
+
+seed();
