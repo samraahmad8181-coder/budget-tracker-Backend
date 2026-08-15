2745ce224f0649fbf5c37584ca3a58759a6cf68a
samraahmad8181-coder <samraahmad8181@gmail.com>
2026-08-15 00:15:39 -0700

    Update database connection configuration
    Removed SSL configuration and connection log.

--- src/db/db.js
+++ src/db/db.js
@@ -2,11 +2,6 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false },
 });
-
-pool.connect().then(() =>
-    console.log('Database is connected')
-);
 
 module.exports = pool;
