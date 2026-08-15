b3926ef9193ee2dfe393af4a259098b9a3e88357
Samra Ahmad <samraahmad8181@gmail.com>
2026-08-15 03:38:03 -0700

    Add seed file

--- src/db/db.js
+++ src/db/db.js
@@ -4,8 +4,4 @@ const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
 });
 
-<<<<<<< HEAD
 module.exports = pool;
-=======
-module.exports = pool;
->>>>>>> ce4596a9d68f77e9e8969cf9567224de17d6b6f7
