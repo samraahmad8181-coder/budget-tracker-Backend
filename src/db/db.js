5597cb5d015093bc9456b900ae1b39a94996df51
samraahmad8181-coder <samraahmad8181@gmail.com>
2026-08-15 00:06:05 -0700

    Refactor database connection to use environment variable

--- src/db/db.js
+++ src/db/db.js
@@ -1,23 +1,12 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "tracker",
-    password: "8862",
-    port: 5432,
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
 });
 
 pool.connect().then(() =>
     console.log('Database is connected')
 );
-
-// pool.query("SELECT NOW()", (err, result) => {
-//     if (err) {
-//         console.error(err);
-//     } else {
-//         console.log(result.rows);
-//     }
-// });
 
 module.exports = pool;
