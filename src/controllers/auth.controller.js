0c6942b3598d16793ee7ebfd92312816ebf943d5
samraahmad8181-coder <samraahmad8181@gmail.com>
2026-08-22 02:27:42 -0700

    Update cookie settings for token in auth controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -54,7 +54,8 @@ const loginUser = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: "lax",
+            secure: true,
+            sameSite: "none",
             maxAge: 24 * 60 * 60 * 1000,
         });
 
